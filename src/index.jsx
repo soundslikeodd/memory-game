2d6e0ae773629698cbfafaeebe13e847a16defd6
soundlikeodd <mattschaadt@gmail.com>
2023-06-08 20:39:11 -0700

    feat: added package info to web console

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { webPackageBanner } from '@soundslikeodd/package-banner';
+import standard from 'figlet/importable-fonts/Standard'; // eslint-disable-line  import/no-unresolved
+import packageJson from '../package.json';
 import Memory from './Memory';
+
+webPackageBanner({
+  packageJson,
+  capitalCase: true,
+  breakOnWord: true,
+  figletFontFileData: standard,
+  additionalPackageInfo: [
+    'description',
+    'author',
+    'license',
+  ],
+});
 
 ReactDOM.render(
   <Memory />,
